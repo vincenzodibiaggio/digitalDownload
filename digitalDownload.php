ec2e4c6af6649232bbcff7d7803a4122487a2220
vincenzodibiaggio <aniceweb@gmail.com>
2013-03-20 09:10:05 +0100

    license

--- digitalDownload.php
+++ digitalDownload.php
@@ -1,4 +1,38 @@
 <?php
+/**
+* @license
+*
+* digitalDownload
+*
+* Copyright (c) 2013 Vincenzo Di Biaggio
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* 		* Redistributions of source code must retain the above copyright
+* 			notice, this list of conditions and the following disclaimer.
+* 		* Redistributions in binary form must reproduce the above copyright
+* 			notice, this list of conditions and the following disclaimer in the
+* 			documentation and/or other materials provided with the distribution.
+* 		* Neither the name of the <organization> nor the
+* 			names of its contributors may be used to endorse or promote products
+* 			derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* @author vincenzodb - vincenzodibiaggio - aniceweb@gmail.com - http://vincenzodb.com/
+*
+*/
+
 namespace DigitalDownload;
 
 require 'extSrc/writeOverImage.php';
@@ -43,10 +77,10 @@ class DigitalDownload
 	public $limitNumDownload = 5; // how many time a code is valid? 0 = unlimited 
 	public $limitByHour = 2; // hour numbers from first use to expire 0 = unlimited
 	
-	const DD_DB_HOST = 'localhost';
-	const DD_DB_USER = 'root';
-	const DD_DB_PASS =  'fr3k3t3';
-	const DD_DB_DATABASE = 'file_download_manager';
+	const DD_DB_HOST = 'host';
+	const DD_DB_USER = 'user';
+	const DD_DB_PASS =  'pass';
+	const DD_DB_DATABASE = 'database';
 	const DD_DB_CODES_TABLE = 'DD_codes';
 	const DD_DB_LOG_TABLE = 'DD_downloads_logger';
 	
